b8a0ac2da5bfcc6a56dee24b6bd72e9b9c44908b
boboiazumi <boboi0704@gmail.com>
2025-03-15 22:37:57 +0700

    feat: table list

--- frontend-service/src/pages/sub/list-sk.tsx
+++ frontend-service/src/pages/sub/list-sk.tsx
@@ -72,86 +72,66 @@ export function ListSK(){
                                 <option value={100}>100</option>
                             </select>
                         </div>
-                        {
-                            documentResponse.map((v:DocumentTypes, i:number) => (
-                                <div className="w-full bg-[#f2f2f2] p-4 rounded-md mb-4" key={v.id}>
-                                    <h6 className="font-semibold mb-5 items-center">
-                                        {v.title}
-                                    </h6>
-                                    <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
-                                        <table className="table">
-                                            <tbody>
-                                                <tr>
-                                                    <td>Jenis</td>
-                                                    <td>
-                                                        : {v.documentType}
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>
-                                                        Tahun Ajaran
-                                                    </td>
-                                                    <td>
-                                                        : {v.academicYear}
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>
-                                                        Semester
-                                                    </td>
-                                                    <td>
-                                                        : {v.semester}
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>
-                                                        Diupload oleh
-                                                    </td>
-                                                    <td>
-                                                        : {v.ByUser.fullName}
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>
-                                                        Kepada
-                                                    </td>
-                                                    <td>
-                                                        : {v.ToUser.fullName}
-                                                    </td>
-                                                </tr>
-                                                <tr>
-                                                    <td>
-                                                        Tanggal
-                                                    </td>
-                                                    <td>
-                                                        : {ParseDate(v.createdAt)}
-                                                    </td>
-                                                </tr>
-                                            </tbody>
-                                        </table>
-                                    </div>
-                                    <h6 className="mt-5 font-semibold">Files</h6>
-                                    {v.File.map((file) => (
-                                        <div className="flex justify-between items-center mb-5 gap-3 overflow-auto">
-                                            <h6>{file.fileName}</h6>
-                                            <a href={`${BackendUrl}${file.location}`} target="_blank" className="btn btn-info">Download</a>
-                                        </div>
-                                    ))}
-                                    {account.role != "ADMIN" ?
-                                        v.ToUser.id != v.ByUser.id ? 
-                                        (<></>) : (
-                                            <button className="btn btn-error" onClick={() => deleteDocument(i)}>
-                                                Delete Document
-                                            </button>
-                                        )
-                                    : (
-                                        <button className="btn btn-error" onClick={() => deleteDocument(i)}>
-                                            Delete Document
-                                        </button>
-                                    )}
-                                </div>
-                            ))
-                        }
+                        <div className="overflow-x-auto">
+                            <table className="table table-zebra">
+                                <thead>
+                                    <tr>
+                                        <th></th>
+                                        <th>Judul</th>
+                                        <th>Jenis</th>
+                                        <th>Tahun Ajaran</th>
+                                        <th>Semester</th>
+                                        <th>Diupload Oleh</th>
+                                        <th>Kepada</th>
+                                        <th>Tanggal</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        documentResponse.map((v:DocumentTypes, i:number) => (
+                                            <tr>
+                                                <td>{(i + (limit * (page -  1))) + 1}</td>
+                                                <td>
+                                                    <p className="min-w-[7rem]">{v.title}</p>
+                                                </td>
+                                                <td>{v.documentType}</td>
+                                                <td>{v.academicYear}</td>
+                                                <td>{v.semester}</td>
+                                                <td>{v.ByUser.fullName}</td>
+                                                <td>{v.ToUser.fullName}</td>
+                                                <td>{ParseDate(v.createdAt)}</td>
+                                                <td>
+                                                    <div className="flex flex-col gap-2">
+                                                        <button 
+                                                            className="btn btn-info"
+                                                            onClick={() => {
+                                                                setSelectedDocument(i)
+                                                                const modal = (document.getElementById("view_modal") as HTMLFormElement)
+                                                                modal?.showModal()
+                                                            }}
+                                                        >View
+                                                        </button>
+                                                        {account.role != "ADMIN" ?
+                                                            v.ToUser.id != v.ByUser.id ? 
+                                                            (<></>) : (
+                                                                <button className="btn btn-error" onClick={() => deleteDocument(i)}>
+                                                                    Delete
+                                                                </button>
+                                                            )
+                                                            : (
+                                                                <button className="btn btn-error" onClick={() => deleteDocument(i)}>
+                                                                    Delete
+                                                                </button>
+                                                        )}
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        ))
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
                         {documentResponse.length == 0 ? (
                             <h4 className="text-center my-10">Empty</h4>
                         ) : (
@@ -184,6 +164,80 @@ export function ListSK(){
                         <form method="dialog" className="flex gap-2">
                             <button className="btn">Close</button>
                             <button className="btn btn-error" onClick={() => confirmDelete(documentResponse[selectedDocument].id)}>Yes</button>
+                        </form>
+                    </div>
+                </div>
+            </dialog>
+
+            <dialog id="view_modal" className="modal modal-bottom sm:modal-middle">
+                <div className="modal-box max-h-[75vh] overflow-y-auto">
+                    <div className="w-full bg-[#f2f2f2] p-4 rounded-md mb-4">
+                        <h6 className="font-semibold mb-5 items-center">
+                            {documentResponse[selectedDocument]?.title}
+                        </h6>
+                        <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+                            <table className="table">
+                                <tbody>
+                                    <tr>
+                                        <td>Jenis</td>
+                                        <td>
+                                            : {documentResponse[selectedDocument]?.documentType}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Tahun Ajaran
+                                        </td>
+                                        <td>
+                                            : {documentResponse[selectedDocument]?.academicYear}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Semester
+                                        </td>
+                                        <td>
+                                            : {documentResponse[selectedDocument]?.semester}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Diupload oleh
+                                        </td>
+                                        <td>
+                                            : {documentResponse[selectedDocument]?.ByUser.fullName}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Kepada
+                                        </td>
+                                        <td>
+                                            : {documentResponse[selectedDocument]?.ToUser.fullName}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            Tanggal
+                                        </td>
+                                        <td>
+                                            : {ParseDate(documentResponse[selectedDocument]?.createdAt)}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <h6 className="mt-5 font-semibold">Files</h6>
+                        {documentResponse[selectedDocument]?.File.map((file) => (
+                            <div className="flex justify-between items-center mb-5 gap-3 overflow-auto">
+                                <h6>{file.fileName}</h6>
+                                <a href={`${BackendUrl}${file.location}`} target="_blank" className="btn btn-info">Download</a>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="modal-action">
+                        <form method="dialog">
+                            <button className="btn">Close</button>
                         </form>
                     </div>
                 </div>
